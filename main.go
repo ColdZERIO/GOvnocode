89e1c7f7b5c5b5e0a223c5c3f2831798374aca68
Tkachenko Dmitriy <TifaLuv@yandex.ru>
2025-10-14 19:54:37 +0300

    VSE RESHILI SUKA BLYAT

--- main.go
+++ main.go
@@ -17,11 +17,10 @@ func main() {
 
 	Zslice := []int{5, 3, 7, 8, 4, 5, 75, 34, 7, 5, 543, 4, 6, 5, 7, 4, 45, 6}
 
-<<<<<<< HEAD
 	// Сань, какая-то залупа с этой функцией. Чекни папку Hui
 	res := hui.ChekSLice(Zslice)
 	fmt.Println(res)
-=======
+
 	hui.сhekSLice() // Сань, какая-то залупа с этой функцией. Чекни папку Hui
 
 	err := zlp.Div()
@@ -29,5 +28,4 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
->>>>>>> 60225619dce883a957c50a42b0671df970e29012
 }
